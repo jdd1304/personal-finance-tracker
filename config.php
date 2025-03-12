6c4984377dedc0656f088c16deb74ac9d2e212e1
Heet1337x <heetvachhani1337@gmail.com>
2025-03-12 12:09:19 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-// Database connection settings
+
 $servername = "localhost";
 $username = "root"; 
 $password = "";
 $dbname = "finance_tracker"; 
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
